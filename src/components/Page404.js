7b708ad9ae3ae06a1e6f512d60c4b07c303df7e4
Robert Pirritano <rpirritano@gmail.com>
2018-08-18 12:33:42 -0700

    incorported react router

---
+++ src/components/Page404.js
@@ -0,0 +1,4 @@
+export const Page404 = () =>
+  <div>
+    <h1>Whoops, page not found!</h1>
+  </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import './stylesheets/ui.scss'
 import { App } from './components/App'
+import { Page404 } from './components/Page404'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
 render(
-	<App />,
+	<Router history={hashHistory}>
+		<Route path="/" component={App}/>
+		<Route path="*" component={Page404}/>
+	</Router>,
 		document.getElementById('react-container')
 	)
