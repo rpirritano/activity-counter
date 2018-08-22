8cff753ed99c6a48d38c70b9d87fc0292564c828
Robert Pirritano <rpirritano@gmail.com>
2018-08-21 19:22:07 -0700

    added refs to form

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,29 +1,42 @@
 import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
+
+  constructor(props) {
+    super(props)
+    this.submit = this.submit.bind(this)
+  }
+
+  submit(e) {
+    e.preventDefault()
+    console.log('routine', this.refs.routine.value)
+    console.log('date', this.refs.date.value)
+    console.log('weights', this.refs.weights.checked)
+    console.log('cardio', this.refs.cardio.checked)
+  }
   render() {
 
     const { routine, date, weights, cardio } = this.props
 
     return (
-      <form className="add-day-form">
+      <form onSubmit={this.submit} className="add-day-form">
 
         <label htmlFor="routine">Routine:</label>
-        <input id="routine" type="text" required defaultValue={routine} />
+        <input id="routine" type="text" required defaultValue={routine} ref="routine" />
 
         <label htmlFor="date">Date:</label>
-        <input id="date" type="date" required defaultValue={date} />
+        <input id="date" type="date" required defaultValue={date} ref="date" />
 
         <div>
           <label htmlFor="weights">Weights:</label>
-          <input id="weights" type="checkbox" defaultChecked = {weights} />
+          <input id="weights" type="checkbox" defaultChecked = {weights} ref="weights" />
         </div>
 
         <div>
           <label htmlFor="cardio">Cario:</label>
-          <input id="cardio" type="checkbox" defaultChecked={cardio} />
+          <input id="cardio" type="checkbox" defaultChecked={cardio} ref="cardio" />
         </div>
-
+        <button>Add Exercise</button>
       </form>
     )
   }
