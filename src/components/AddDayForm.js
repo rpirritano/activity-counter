c88d922d7e9916b8d3542b86e9d212eb95355c67
Robert Pirritano <rpirritano@gmail.com>
2018-08-21 19:52:50 -0700

    converted add form to statless component

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,46 +1,56 @@
 import { PropTypes, Component } from 'react'
 
-export class AddDayForm extends Component {
+export const AddDayForm = ({ routine, date, weights, cardio }) => {
 
-  constructor(props) {
-    super(props)
-    this.submit = this.submit.bind(this)
-  }
+  let _routine, _date, _weights, _cardio
 
-  submit(e) {
+  const submit = (e) => {
     e.preventDefault()
-    console.log('routine', this.refs.routine.value)
-    console.log('date', this.refs.date.value)
-    console.log('weights', this.refs.weights.checked)
-    console.log('cardio', this.refs.cardio.checked)
-  }
-  render() {
-
-    const { routine, date, weights, cardio } = this.props
-
-    return (
-      <form onSubmit={this.submit} className="add-day-form">
-
-        <label htmlFor="routine">Routine:</label>
-        <input id="routine" type="text" required defaultValue={routine} ref="routine" />
-
-        <label htmlFor="date">Date:</label>
-        <input id="date" type="date" required defaultValue={date} ref="date" />
-
-        <div>
-          <label htmlFor="weights">Weights:</label>
-          <input id="weights" type="checkbox" defaultChecked = {weights} ref="weights" />
-        </div>
-
-        <div>
-          <label htmlFor="cardio">Cario:</label>
-          <input id="cardio" type="checkbox" defaultChecked={cardio} ref="cardio" />
-        </div>
-        <button>Add Exercise</button>
-      </form>
-    )
+    console.log('routine', _routine.value)
+    console.log('date', _date.value)
+    console.log('weights', _weights.checked)
+    console.log('cardio', _cardio.checked)
   }
 
+  return (
+		<form onSubmit={submit} className="add-day-form">
+
+			<label htmlFor="routine">Routine Name</label>
+			<input id="routine"
+				   type="text"
+				   required
+				   defaultValue={routine}
+				   ref={input => _routine = input}/>
+
+			<label htmlFor="date">Date</label>
+			<input id="date"
+				   type="date"
+				   required
+				   defaultValue={date}
+				   ref={input => _date = input}/>
+
+			<div>
+				<input id="weights"
+					   type="checkbox"
+					   defaultChecked={weights}
+					   ref="weights"
+					   ref={input => _weights = input}/>
+				<label htmlFor="powder">Weights Day</label>
+			</div>
+
+			<div>
+				<input id="cardio"
+					   type="checkbox"
+					   defaultChecked={cardio}
+					   ref="cardio"
+					   ref={input => _cardio = input}/>
+				<label htmlFor="cardio">
+					Cardio Day
+				</label>
+			</div>
+			<button>Add Day</button>
+		</form>
+	)
 }
 
 AddDayForm.defaultProps = {
